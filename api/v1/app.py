2a545fb749220a00c92165f2dfa99e111567edb0
Solomon <solomonf1227@gmail.com>
2024-08-06 16:18:36 +0300

    API is started: Status

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,6 @@ def close_session():
 
 if __name__ == '__main__':
 
-    host = os.environ.get(HBNB_API_HOST, '0.0.0.0')
-    port = int(os.environ.get(HBNB_API_PORT, 5000))
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 '''Initialize a module and create a flask bluprint'''
 
 from flask import Blueprint
-from api.v1.views.index import *
-
 
 app_views = Blueprint('app_views', url_prefix='/api/v1/')
+
+from api.v1.views.index import *
