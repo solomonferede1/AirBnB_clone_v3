4d9f604f25ca553308f7d29573c50a45cd78df7d
Solomon <solomonf1227@gmail.com>
2024-08-06 16:39:36 +0300

    Shebang

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,7 +17,7 @@ def close_session():
 
 
 if __name__ == '__main__':
-
+    '''Run if main file'''
     host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(os.environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
