0ef3bf6448f0e1b4de5c7569d98248b6a016c83c
Solomon <solomonf1227@gmail.com>
2024-08-07 15:23:15 +0300

    Object count by numbers

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,15 +6,14 @@ from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 from models.amenity import Amenity
-from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": "User"}
+classes = {"Amenities": Amenity, "Cities": City,
+           "Places": Place, "Reviews": Review, "States": State, "Users": User}
 
 
 @app_views.route('/status')
