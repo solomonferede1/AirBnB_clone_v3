1f91ea0ad51635420db6314c3679057f31003882
Solomon <solomonf1227@gmail.com>
2024-08-07 14:30:37 +0300

    Object count by classes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,26 +5,31 @@
 from models import storage
 from flask import jsonify
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": "User"}
 
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
 
-
 @app_views.route('/stats')
 def get_stats():
-    '''The number of objects in each class'''
+    """Counts the number of objects in each class and returns a JSON."""
     counts = {}
-    classes = {
-        "Amenity": "amenities",
-        "City": "cities",
-        "Place": "places",
-        "Review": "reviews",
-        "State": "states",
-        "User": "users"
-    }
-    for cls in classes:
-        count = storage.count(cls)
-        counts[classes.get(cls)] = count
+    for class_name, model in classes.items():
+        try:
+            count = storage.count(model)
+            counts[class_name] = count
+        except Exception as e:
+            pass
+
     return jsonify(counts)
