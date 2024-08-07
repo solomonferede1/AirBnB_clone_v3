136dfa5aa3418c326fb1973cfd143ee2a36ce21d
Solomon <solomonf1227@gmail.com>
2024-08-07 10:12:51 +0300

    Blueprint register correction

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,7 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix='/api/v1/')
 
 
 @app.teardown_appcontext
