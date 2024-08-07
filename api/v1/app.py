a1ccc965daa2ee50e4920fab748fc6f64d0ae105
Solomon <solomonf1227@gmail.com>
2024-08-07 15:34:27 +0300

    Error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
 '''This module Starts a flask web app for HBNB clone project'''
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1/')
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
 def close_session(exception=None):
     '''Ensure the session is closed'''
     storage.close()
+
+
+@app.errorhandler(404)
+def error_404(error):
+    '''Handle 404 error'''
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
